c62dfb72116e54c0912ca0c072b6a67d16307de0
Krishna Chaitanya Parimi <cparimi@codeaurora.org>
2014-05-08 10:53:28 +0530

    msm8916-common: Enable CABL by default
    Enable display postprocessing ABA CABL by default on msm8916.
    
    Change-Id: Ie928a35713ba40dc057525e42bad829da59ba6d8

--- system_prop.mk
+++ system_prop.mk
@@ -42,7 +42,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.cpurend.vsync=false \
     persist.hwc.mdpcomp.enable=1 \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.qualcomm.cabl=2
 
 # FRP
 #PRODUCT_PROPERTY_OVERRIDES += \
