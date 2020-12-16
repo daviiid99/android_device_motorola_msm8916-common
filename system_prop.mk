88841a107b10f61502fb39d01100be8f127e47b2
Kyle Harrison <khwebmail@gmail.com>
2020-12-16 20:16:16 +0000

    msm8916-common: LiveDisplay updates
    - Drop LiveDisplay legacymm
    - Remove IPictureAdjustment
    - Drop mm-pp-daemon & CABL prop

--- system_prop.mk
+++ system_prop.mk
@@ -42,8 +42,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.cpurend.vsync=false \
     persist.hwc.mdpcomp.enable=1 \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.qualcomm.cabl=2
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
 
 # FRP
 #PRODUCT_PROPERTY_OVERRIDES += \
