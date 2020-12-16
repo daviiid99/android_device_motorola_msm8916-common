8606ac9037f5f964a68ee67f9aee2f913b8cecb9
Arne Coucheron <arco68@gmail.com>
2020-12-16 14:59:13 +0000

    msm8916-common: Use in-process APK for network stack and tethering
    Taken from common go configuration, and the purpose is
    to save some memory.
    
    Change-Id: I32222daf8fa2be1f41a8177b82fa6f2f94e002e7

--- msm8916.mk
+++ msm8916.mk
@@ -348,6 +348,10 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
+# Do not spin up a separate process for the network stack, use an in-process APK.
+PRODUCT_PACKAGES += InProcessNetworkStack
+PRODUCT_PACKAGES += com.android.tethering.inprocess
+
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 $(call inherit-product-if-exists, vendor/motorola/msm8916-common/msm8916-common-vendor.mk)
