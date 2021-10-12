c6f93f353065800253f3b0b079fca57cc10dacf2
Akash Mondal <a98mondal@gmail.com>
2021-10-12 16:39:27 +0100

    msm8916-common: Build mock power stats hal
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -301,7 +301,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # USB
 PRODUCT_PACKAGES += \
