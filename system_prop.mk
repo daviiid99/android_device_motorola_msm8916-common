de12d858153e6d4bb8bb89317f5e0451c1533ec7
Kyle Harrison <khwebmail@gmail.com>
2021-10-14 10:00:52 +0100

    msm8916-common: Use threaded GLES render engine

--- system_prop.mk
+++ system_prop.mk
@@ -42,6 +42,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.cpurend.vsync=false \
     persist.hwc.mdpcomp.enable=1 \
+    debug.renderengine.backend=threaded \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
 
 # FRP
